70b43c5c020cbf2ef083ae6a13b7f7a3ac994bf0
Li0n-0 <selekta.lion@live.fr>
2016-11-29 16:40:45 +0100

    Good Working Version
    Light now blink in a Morse Code fashion.
    Settings for Morse Code and disabling the Light action group thingy
    Removed MM and patch
    Bundled CrewLight.csproj and CrewLight.sin

--- GameData/CrewLight/Source/ModuleLightNoAG.cs
+++ GameData/CrewLight/Source/ModuleLightNoAG.cs
@@ -14,22 +14,35 @@ namespace CrewLight
 		 * with MM.
 		 * 
 		 */
+
+		private bool isEnabled;
+
 		void Start ()
 		{
-			GameEvents.onEditorPartEvent.Add (CheckForLight);
+			Settings settings = new Settings ();
+			settings.Load ();
+			isEnabled = settings.disableAutoAG;
+			if (isEnabled) {
+				GameEvents.onEditorPartEvent.Add (CheckForLight);
+			}
 		}
 
 		void OnDestroy ()
 		{
-			GameEvents.onEditorPartEvent.Remove (CheckForLight);
+			if (isEnabled) {
+				GameEvents.onEditorPartEvent.Remove (CheckForLight);
+			}
 		}
 
 		void CheckForLight (ConstructionEventType constrE, Part part)
 		{
 			if (constrE == ConstructionEventType.PartCreated) {
 				if (part.CrewCapacity > 0) {
-					if (part.Modules.Contains<ModuleLight>() || part.Modules.Contains("WBILight")) {
-						/* Other light modules are neutralize with MM */
+					if (part.Modules.Contains<ModuleColorChanger>() 
+						|| part.Modules.Contains<ModuleLight>() 
+						|| part.Modules.Contains<ModuleAnimateGeneric>() 
+						|| part.Modules.Contains("WBILight")) {
+
 						foreach (PartModule partM in part.Modules) {
 							if (partM.Actions.Contains(KSPActionGroup.Light)) {
 								foreach (BaseAction action in partM.Actions) {
