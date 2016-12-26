4932da115ebc65cfc17bdf4be3c11e790b7b101f
Li0n-0 <selekta.lion@live.fr>
2016-12-26 12:29:13 +0100

    Toggle Light in EVA
    First working draft

---
+++ Source/ModuleLightToggle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace CrewLight
+{
+	public class ModuleLightToggle : PartModule
+	{
+		private ModuleLight moduleLight;
+
+		public override void OnStart (StartState state)
+		{
+			moduleLight = part.Modules.GetModule<ModuleLight> ();
+		}
+
+		[KSPEvent (active = true, guiActiveUnfocused = true, externalToEVAOnly = true, guiName = "Toggle Light")]
+		public void LightToggleEVA ()
+		{
+			if (moduleLight.isOn) {
+				moduleLight.LightsOff ();
+			} else {
+				moduleLight.LightsOn ();
+			}
+		}
+	}
+}
+
